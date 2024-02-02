6190ca8912718a59704a14ec69a130524af17e20
Aidan McKenna <aidantmckenna@gmail.com>
2024-02-02 14:17:20 -0500

    debugger

--- src/dmx_app/__init__.py
+++ src/dmx_app/__init__.py
@@ -1,12 +1,14 @@
 from PyDMXControl.controllers import FTDIController
+from PyDMXControl.controllers.utils.debug import Debugger
 from dmx_app import functs
 
 
 def run():
 
     dmx = FTDIController("B002FETU")
+    debugger = Debugger(dmx,)
 
-    functs.colour_after_time(dmx)
+    functs.color_after_time(dmx)
 
     dmx.sleep_till_enter()
     dmx.close()
