c4c731fde7e976e785defc9ec73e7d3e04d5fe7a
Aidan McKenna <aidantmckenna@gmail.com>
2024-02-02 16:04:54 -0500

    fix

--- src/dmx_app/__init__.py
+++ src/dmx_app/__init__.py
@@ -8,7 +8,7 @@ def run():
     dmx = FTD2XXController("B002FETU")
     debugger = Debugger(dmx)
 
-    f = dmx.add_fixture(Spotlight, "spotlight", start_channel=2)
+    f = dmx.add_fixture(Spotlight, "spotlight", start_channel=1)
     f.dim(255, 0, channel="base")
     f.dim(255, 2000, channel="r")
 
