191e4dfd186ed8fb25c9a96e8dd9a6b5746905ad
Aidan McKenna <aidantmckenna@gmail.com>
2024-02-05 12:14:28 -0500

    web control

--- src/dmx_app/__init__.py
+++ src/dmx_app/__init__.py
@@ -6,11 +6,11 @@ from dmx_app.my_dmx import Spotlight
 def run():
 
     dmx = FTD2XXController(0)
-    debugger = Debugger(dmx)
 
     f = dmx.add_fixture(Spotlight, "spotlight", start_channel=1)
     f.dim(255, 0, channel="base")
     f.dim(255, 2000, channel="r")
 
+    dmx.web_control()
     dmx.sleep_till_enter()
     dmx.close()
