494a4e32dab6ee92c2469b28e77b077f6cf29e7f
Aidan McKenna <aidan@montra.com>
2024-02-07 12:05:04 -0500

    fix

--- test/test_service.py
+++ test/test_service.py
@@ -8,7 +8,7 @@ import time
 def test_load_config():
     service = DMXService()
     service.load_universe("config/test_universe.yaml")
-    service.set_global_color(Colors.RED)
+    service.set_global_color(Colors.Red)
 
     for i in range(100):
         time.sleep(0.1)
