fba474b9ea1c1f90063a09b3c222dbbc0041855e
Aidan McKenna <aidan@montra.com>
2024-02-07 14:18:36 -0500

    fix

--- src/dmx_app/web/api.py
+++ src/dmx_app/web/api.py
@@ -8,11 +8,10 @@ from dmx_app import Colors
 
 api_routes = Blueprint("api", __name__, url_prefix="/")
 
-service = DMXService()
-
 @api_routes.route("/global/color/<color>", methods=['PUT'])
 def set_global_color(color):
     color = getattr(Colors, color)
+    print(color)
     current_app.parent.dmx_service.set_global_color(color)
     return "OK"
 
