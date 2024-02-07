4254a34b9da07b7532b44bede59b02519966d00c
Aidan McKenna <aidan@montra.com>
2024-02-07 12:14:15 -0500

    fix

--- src/dmx_app/fixtures.py
+++ src/dmx_app/fixtures.py
@@ -2,27 +2,22 @@ from PyDMXControl.profiles.defaults import Fixture
 from PyDMXControl.profiles.Generic import RGB_Vdim, Dimmer
 import sys
 
-from typing import Tuple, Union
 
 class Spotlight(Fixture):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._register_channel('base')
         self._register_channel('red')
-        self._register_channel_aliases('red', 'r')
+        self._register_channel_aliases('red', 'r', 'R')
         self._register_channel('green')
-        self._register_channel_aliases('green', 'g')
+        self._register_channel_aliases('green', 'g', 'G')
         self._register_channel('blue')
-        self._register_channel_aliases('blue', 'b')
+        self._register_channel_aliases('blue', 'b', 'B')
         self._register_channel('white')
-        self._register_channel_aliases('white', 'w')
+        self._register_channel_aliases('white', 'w', 'W')
         self._register_channel('effect')
         self.dim(255, channel="base")
         self.dim(0, channel='effect')
-
-    def anim(self, milliseconds: int, *channels_values: Tuple[Union[str, int], int]):
-        for channel_value in ["r", "g", "b"]:
-            self.dim(channel_value[1], milliseconds, channel_value[0])
 
 
 def from_str(class_name):
