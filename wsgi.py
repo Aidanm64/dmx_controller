35a94f1d614cd7f6e36273c91d7f02cdc2bc5d60
Aidan McKenna <aidan@montra.com>
2024-02-07 14:25:20 -0500

    wait

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
 
 from dmx_app.web.web_controller import WebController
 from dmx_app.service import DMXService
+import time
 
 service = DMXService()
 
 web_controller = WebController(dmx_service=service)
 
-service.dmx.sleep_till_enter()
+while True:
+    try:
+        time.sleep(0.001)
+    except KeyboardInterrupt:
+        break
+
+service.dmx.close()
